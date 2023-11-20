c64c4a0747eeb4771118736adfa5f1d3a5d2b3b2
Keon Kim <fnc6488@gmail.com>
2023-11-21 05:53:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class = "name">
+    {{ name }}
+  </div>
+  <button class="btn btn-primary" @click="updateName">click</button>
+</template>
+
+<script setup>
+import {ref} from "vue"
+import {reactive} from "vue"
+
+  const name = reactive({
+    id: 1
+  });
+
+  const updateName = () => {
+    name.id += 1;
+  }
+
+</script>
+
+<style scoped>
+</style>
