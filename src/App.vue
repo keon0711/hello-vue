739b4d4e74ea163e32aca0a16d61febb25be7c49
Keon Kim <fnc6488@gmail.com>
2023-12-07 12:14:04 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
-  <div class = "name">
-    {{ name }}
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input class="form-control" type="text" v-model="todo" placeholder="Type new To-Do">
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>
+        </div>
+      </div>
+      <div v-show="hasError" style="color: red;">This field cannot be empty</div>
+    </form>
+    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
+      <div class="card-body p-2">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" v-model="todo.completed">
+          <label class="form-check-label" for="">{{ todo.subject }}</label>
+        </div>
+      </div>
+    </div>
+    
   </div>
-  <button class="btn btn-primary" @click="updateName">click</button>
+
 </template>
 
 <script setup>
 import {ref} from "vue"
-import {reactive} from "vue"
 
-  const name = reactive({
-    id: 1
-  });
+  const todo = ref("");
+  const todos = ref([]);
+  const hasError = ref(false);
 
-  const updateName = () => {
-    name.id += 1;
+  const onSubmit = () => {
+    if (todo.value === '') {
+      hasError.value = true;
+    } else {
+      hasError.value = false;
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+        completed: false
+      })
+    }
   }
-
 </script>
 
 <style scoped>
